8ac28e6b384891d2fb3e1b7f660ecc4eccdef243
github username <github email address>
2016-12-25 17:16:44 -0800

    finished book redux app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return[
+    { title: 'Javascript: The Good Parts', pages: 1 },
+    { title: 'Harry Potter', pages: 3 },
+    { title: 'The Dark Tower', pages: 4 },
+    { title: 'Eloquent Ruby', pages: 5 }
+  ]
+}
