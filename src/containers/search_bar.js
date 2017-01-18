8bfd1aac2d156b98387a391d16a610f2e1a6d666
github username <github email address>
2017-01-17 21:47:34 -0500

    weather app

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
 
+  constructor(props){
+    super(props);
+
+    this.state = { term: ""};
+  }
+
   render(){
     return (
       <form className="input-group">
